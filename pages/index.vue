28dd4005e9f757a5a7c25b3aa71ae471bfae72c5
Saurabh Srivastava <me@saurabhsrivastava.dev>
2021-07-26 01:23:50 +0530

    Making header padding responsive with {screen}: prefix

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-2 flex justify-center bg-red-800">
-    <header class="bg-red-200 px-32 py-4 rounded font-bold text-red-800">
+    <header class="bg-red-200 md:px-32 px-16 py-4 rounded font-bold text-red-800">
       Coding Toolbelt
     </header>
   </div>
