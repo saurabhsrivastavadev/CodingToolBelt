02a0f91e73ddaeeb8acc4e3dec0d8e92cd97a00f
Saurabh Srivastava <me@saurabhsrivastava.dev>
2021-08-31 13:21:36 +0530

    Adding Google Fonts to tailwind

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex justify-center my-2">
-    <h1 class="w-max font-serif text-2xl">{ Coding Toolbelt }</h1>
+    <h1 class="w-max font-serif text-4xl text-red-800 font-berkshire">{ Coding Toolbelt }</h1>
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          'berkshire': ['"Berkshire Swash"', 'cursive']
+        }
+      }
+    }
+  };
