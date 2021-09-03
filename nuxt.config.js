837e45431c54fd12b82cdc8681b1551cba2a6069
Saurabh Srivastava <me@saurabhsrivastava.dev>
2021-09-03 12:35:45 +0530

    css file from assets not working in production

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,13 +15,13 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/css/main.css' }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/css/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
