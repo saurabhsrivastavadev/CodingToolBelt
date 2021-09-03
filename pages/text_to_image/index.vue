d8a7dc1b78f00db2a2109211c1c7e3ecc066fd67
Saurabh Srivastava <me@saurabhsrivastava.dev>
2021-09-03 12:36:08 +0530

    Added text positioning support on canvas

--- pages/text_to_image/index.vue
+++ pages/text_to_image/index.vue
@@ -9,12 +9,30 @@
     <section class="mt-12">
       <div class="flex">
         <div class="flex flex-col mr-2">
-          <label class="font-berkshire">Canvas Height</label>
-          <input v-model="canvasHeight" placeholder="Canvas Height" name="canvasHeight" class="p-2 border-2 border-red-400">
-        </div>
-        <div class="flex flex-col">
           <label class="font-berkshire">Canvas Width</label>
           <input v-model="canvasWidth" placeholder="Canvas Width" class="p-2 border-2 border-red-400">
+        </div>
+        <div class="flex flex-col">
+          <label class="font-berkshire">Canvas Height</label>
+          <input v-model="canvasHeight" placeholder="Canvas Height" class="p-2 border-2 border-red-400">
+        </div>
+      </div>
+    </section>
+    <section class="mt-2">
+      <div class="flex flex-col items-center">
+        <label class="font-berkshire">Text</label>
+        <input v-model="canvasText" placeholder="Text" size="60" class="p-2 border-2 border-red-400 text-center">
+      </div>
+    </section>
+    <section class="mt-2">
+      <div class="flex">
+        <div class="flex flex-col mr-2">
+          <label class="font-berkshire">Text Left Position</label>
+          <input v-model="canvasTextPosLeft" placeholder="Text Pos Left" class="p-2 border-2 border-red-400">
+        </div>
+        <div class="flex flex-col">
+          <label class="font-berkshire">Text Top Position</label>
+          <input v-model="canvasTextPosTop" placeholder="Text Pos Top" class="p-2 border-2 border-red-400">
         </div>
       </div>
     </section>
@@ -28,17 +46,23 @@ export default {
     return {
       canvasHeight: 200,
       canvasWidth: 500,
+      canvasText: 'Hello World',
+      canvasTextPosLeft: 120,
+      canvasTextPosTop: 80,
+      canvasTextFont: '30px Arial',
     }
   },
   computed: {
     iframeHeight() {
-      return this.canvasHeight + 30;
+      return parseInt(this.canvasHeight) + 30;
     },
     iframeWidth() {
-      return this.canvasWidth + 30;
+      return parseInt(this.canvasWidth) + 30;
     },
     iframeSrc() {
-      return `/iframes/text_to_image?h=${this.canvasHeight}&w=${this.canvasWidth}`;
+      return `/iframes/text_to_image?` +
+      `h=${this.canvasHeight}&w=${this.canvasWidth}&t=${this.canvasText}&` +
+      `tt=${this.canvasTextPosTop}&tl=${this.canvasTextPosLeft}&tf=${this.canvasTextFont}`;
     } 
   }
 }
--- static/iframes/text_to_image/main.js
+++ static/iframes/text_to_image/main.js
@@ -1,11 +1,18 @@
 console.log('iframe :: text to image :: main.js');
 
-// Set canvas dimensions
+// fetch url parameters
 let url = new URL(window.location.href);
 let canvasWidth = url.searchParams.get('w');
 let canvasHeight = url.searchParams.get('h');
+let text = url.searchParams.get('t');
+let textPosTop = url.searchParams.get('tt');
+let textPosLeft = url.searchParams.get('tl');
+let textFont = url.searchParams.get('tf');
 
-// Get canvas
+// setup canvas as per parameters
 let canvas = document.getElementById('canvas');
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
+let ctx = canvas.getContext('2d');
+ctx.font = textFont;
+ctx.fillText(text, textPosLeft, textPosTop);
